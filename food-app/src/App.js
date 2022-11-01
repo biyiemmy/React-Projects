94fb167f60c3ee9410529d8f25195813e21b473d
adebiyiemmanuel11@gmail.com <adebiyiemmanuel11@gmail.com>
2022-11-01 20:28:07 +0100

    food app

---
+++ food-app/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DashBoard from "./pages/DashBoard";
+
+function App() {
+  return (
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
